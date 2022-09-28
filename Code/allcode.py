1a726d5d932b47c116de3b70d1dac630654d94fd
Steven Nazaroff <stnazaroff@yahoo.com>
2022-09-27 22:46:48 -0400

    made some more changes

--- Code/allcode.py
+++ Code/allcode.py
@@ -5,6 +5,7 @@ import datetime as dt  # for timestamp in header of each block
 import hashlib
 import sys
 import os
+import random
 
 
 class Leaf:
@@ -38,11 +39,11 @@ class MerkleTree:
                 acct, bal = line.split(' ', 1)  # split line into two variables
                 if (len(acct) != 40):        # check if address is 40 characters
                     print("\nError: Invalid address at input line " +
-                        str(values.index(line) + 1) + ".\n")
+                          str(values.index(line) + 1) + ".\n")
                     # sys.exit(0)
                 if (not bal.isdigit()):      # check if balance is a number
                     print("\nError: Invalid balance at input line " +
-                        str(values.index(line) + 1) + ".\n")
+                          str(values.index(line) + 1) + ".\n")
                     # sys.exit(0)
                 account.append(acct)    # add address to account array
                 balance.append(bal)     # add balance to balance array
@@ -51,7 +52,6 @@ class MerkleTree:
                 print(line.split(" ", 1))
                 print(values)
                 sys.exit(-1)
-                
 
         # create leafs from the values
         Leafs = [Leaf(None, None, account[i], balance[i], Leaf.hash(
@@ -140,7 +140,10 @@ class MerkleTree:
         self._printTreeGraphically(node.left, level + 1)
 
 # read in each argument in argv adn append it to an array. then we have array of all filesname / paths to files.
-def makeTree(fileInputs):       # reads in fileInputs, which is the list of paths to the different input files
+
+
+# reads in fileInputs, which is the list of paths to the different input files
+def makeTree(fileInputs):
     print("\nInputted files: ", fileInputs, "\n")
     for files in fileInputs:
         try:
@@ -165,13 +168,13 @@ def makeTree(fileInputs):       # reads in fileInputs, which is the list of path
 if len(sys.argv) == 1:
     print("\nMake a selection \n1. Enter multiple file names \n2. Enter a folder (all files in folder will be entered) \n3. Exit\n")
     selectionInput = input("Selection: ")
-    if(selectionInput == "1"):       # multiple file names
+    if (selectionInput == "1"):       # multiple file names
         print("Enter a single file name one at a time (enter 0 to exit): ")
         fileNames = []      # array to make tree
         fileName = ""       # file location var
         loop = True         # infinite loop unless 0 is entered
-        while(loop):
-            if(fileName == "0"):        # ends loop
+        while (loop):
+            if (fileName == "0"):        # ends loop
                 loop = False
             else:                       # enter values to be added to array. 0 will still be entered at the end, but is removed below
                 fileName = input("\nEnter the path to a file (0 to exit): ")
@@ -179,20 +182,22 @@ if len(sys.argv) == 1:
         # remove last element from array (which would be 0 to exit loop but also got appended to array)
         fileNames.pop()
         makeTree(fileNames)
-    elif(selectionInput == "2"):     # folder
+    elif (selectionInput == "2"):     # folder
+        # prompt for folder name
         print("Enter the name of the folder (must be in same directory): ")
-        folderName = input("Folder Name: ")
-        dirList = os.listdir(folderName)
+        folderName = input("Folder Name: ")    # get folder name
+        dirList = os.listdir(folderName)    # get list of files in folder
+        # add folder name to each file name
         dirList = ["{}/{}".format(folderName, i) for i in dirList]
-        makeTree(dirList)
+        makeTree(dirList)   # make tree from list of files
     else:
-        print("Exitting...")
-        sys.exit(0)
+        print("Exitting...")    # exit program
+        sys.exit(0)    # exit program
 
 # Makes tree if more than 1 argv
 if len(sys.argv) > 1:
-    makeTree(sys.argv[1:])  # make tree for every file by making an array of file inputs for the tree via argv (ignores argv[0] with is python3 and argv[1] which is program name, takes elements onwards)
-
+    # make tree for every file by making an array of file inputs for the tree via argv (ignores argv[0] with is python3 and argv[1] which is program name, takes elements onwards)
+    makeTree(sys.argv[1:])
 
 
 # --------------------------Begin HW 4-------------------------------------
@@ -204,25 +209,22 @@ if len(sys.argv) > 1:
 # • nonce
 
 class Block:
+    
     def __init__(self, hash_prev, hash_root, timestamp, target, nonce):
-        self.hash_prev = hash_prev  #hash of previous block
-        self.hash_root = hash_root  #hash of root of Merkle tree
-        self.timestamp = timestamp
-        self.target = target  # for the difficulty target
-        self.nonce = nonce
-        self.hash = self.compute_hash() #hash of the previous block
-        # self.roothash = self.MerkleTree.getRootHash() #hash of the root of the Merkle tree stored in the current block
-
+        self.hash_prev = hash_prev  # hash of previous block
+        self.hash_root = hash_root  # hash of root of Merkle tree
+        self.timestamp = dt.datetime.now()  # timestamp
+        self.target = 2 ** (256-10)  # for the difficulty target
+        self.nonce = 0  # nonce
 
     def compute_hash(self):
         """
         A function that return the hash of the block contents.
         """
-        hashes = hashlib.sha256()
-        header_data = str(self.hash_prev) + str(self.hash_root) + str(self.timestamp) + str(self.target) + str(self.nonce)
-        hashes.update(header_data.encode('utf-8'))
-        return hashes.hexdigest()
-    
+        hashes = hashlib.sha256(((str(self.hash_prev) + str(self.hash_root) + str(
+            self.timestamp) + str(self.target) + str(self.nonce)).encode('utf-8'))).hexdigest()
+        return hashes
+
     @staticmethod
     def create_genesis_block():
         """
@@ -231,18 +233,57 @@ class Block:
         the timestamp is set to the current time, and the nonce and target
         are set to 0.
         """
-        
+
         return Block("0", "0", dt.datetime.now(), 0, 0)
 
+    def make_nounce(self):
+        # """
+        # A function that returns a nonce that is less than the target.
+        # """
+        # nonce = 0
+        # while True:
+        #     if int(self.compute_hash(), 16) <= self.target:
+        #         break
+        #     else:
+        #         nonce += 1
+        # return nonce
+        curr_nonce = random.randint(0, 2**64)
+        tries = 0
+        while True:
+            tries += 1
+            if int(hashlib.sha256((hex(curr_nonce) + self.hash_root.MerkleTree.getRootHash()).encode('utf-8')).hexdigest(), 16) < self.target:
+                self.nonce = curr_nonce
+                break
+            else:
+                curr_nonce = random.randint(0, 2**64)
+
+# def mine(self, block):
+#         for n in range(self.maxNonce):
+#             if int(block.hash(), 16) <= self.target:
+#                 self.add(block)
+#                 print(block)
+#                 break
+#             else:
+#                 block.nonce += 1
     # You will need to find a nonce such that the nonce concatenated
     # with the root hash of the Merkle tree is hashed by SHA-256 to a value less than or equal to the specified
     # target. Please set your target such that the probability of success is 50%
 
 
 class Blockchain:
-    block_chain = [Block.create_genesis_block()]
-    print("Block1 (Genesis Block): " + str(block_chain[0].hash))
-    print("Hash of previous block:- 0")
+
+    print("Begin Block\n")
+    print("Begin Header\n")
+    print("Genesis Block\n")
+    genesis_block = [Block.create_genesis_block()]
+    print("Hash: "+genesis_block[0].compute_hash() + "\n")
+    print("Hash of root: " + genesis_block[0].hash_prev + "\n")
+    print("Timestamp: " + str(genesis_block[0].timestamp) + "\n")
+    print("Target: " + str(genesis_block[0].target) + "\n")
+    print("Nonce: " + str(genesis_block[0].make_nounce()) + "\n")
+    print("End Header\n")
+    print("End Block\n")
+    print("------------------------\n")
 
     # # nonce =
     # # target =
@@ -279,6 +320,6 @@ class Blockchain:
 #         print("s")
 
 
-genesis_block = Blockchain()
-print(genesis_block)
+# output = Blockchain()
+# print(output)
 # print(genesis_block.compute_hash)
