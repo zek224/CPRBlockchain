4cb8b2782e72df138a37917897c13d8336baa8b0
Steven Nazaroff <stnazaroff@yahoo.com>
2022-09-28 11:40:49 -0400

    added some functions

--- Code/allcode.py
+++ Code/allcode.py
@@ -139,67 +139,6 @@ class MerkleTree:
             print()
         self._printTreeGraphically(node.left, level + 1)
 
-# read in each argument in argv adn append it to an array. then we have array of all filesname / paths to files.
-
-
-# reads in fileInputs, which is the list of paths to the different input files
-def makeTree(fileInputs):
-    print("\nInputted files: ", fileInputs, "\n")
-    for files in fileInputs:
-        try:
-            # try - catch to try to open file
-            file = open(files, "r")
-            print('File opened')
-        except:
-            # if not a file, print this and exit program
-            print("\nError: Please enter a valid text file.\n")
-            sys.exit(0)    # exit program
-
-        array = []
-        for line in file:
-            array.append(line.strip())  # add each line to array
-
-        tree = MerkleTree(array)        # make tree from input array ()
-        print("Root Hash: " + tree.getRootHash() + "\n")
-        # tree.printTreeGraphically()
-
-
-# if "python3 allcode.py" only, then prompt for file inputs
-if len(sys.argv) == 1:
-    print("\nMake a selection \n1. Enter multiple file names \n2. Enter a folder (all files in folder will be entered) \n3. Exit\n")
-    selectionInput = input("Selection: ")
-    if (selectionInput == "1"):       # multiple file names
-        print("Enter a single file name one at a time (enter 0 to exit): ")
-        fileNames = []      # array to make tree
-        fileName = ""       # file location var
-        loop = True         # infinite loop unless 0 is entered
-        while (loop):
-            if (fileName == "0"):        # ends loop
-                loop = False
-            else:                       # enter values to be added to array. 0 will still be entered at the end, but is removed below
-                fileName = input("\nEnter the path to a file (0 to exit): ")
-                fileNames.append(fileName)
-        # remove last element from array (which would be 0 to exit loop but also got appended to array)
-        fileNames.pop()
-        makeTree(fileNames)
-    elif (selectionInput == "2"):     # folder
-        # prompt for folder name
-        print("Enter the name of the folder (must be in same directory): ")
-        folderName = input("Folder Name: ")    # get folder name
-        dirList = os.listdir(folderName)    # get list of files in folder
-        # add folder name to each file name
-        dirList = ["{}/{}".format(folderName, i) for i in dirList]
-        makeTree(dirList)   # make tree from list of files
-    else:
-        print("Exitting...")    # exit program
-        sys.exit(0)    # exit program
-
-# Makes tree if more than 1 argv
-if len(sys.argv) > 1:
-    # make tree for every file by making an array of file inputs for the tree via argv (ignores argv[0] with is python3 and argv[1] which is program name, takes elements onwards)
-    makeTree(sys.argv[1:])
-
-
 # --------------------------Begin HW 4-------------------------------------
 
 # • hash of the header of the previous block (zero for the initial genesis block)
@@ -208,13 +147,15 @@ if len(sys.argv) > 1:
 # • difficulty target
 # • nonce
 
+
 class Block:
-    
+    next = None
+
     def __init__(self, hash_prev, hash_root, timestamp, target, nonce):
         self.hash_prev = hash_prev  # hash of previous block
         self.hash_root = hash_root  # hash of root of Merkle tree
         self.timestamp = dt.datetime.now()  # timestamp
-        self.target = 2 ** (256-10)  # for the difficulty target
+        self.target = 2 ** (256-50)  # for the difficulty target
         self.nonce = 0  # nonce
 
     def compute_hash(self):
@@ -236,7 +177,7 @@ class Block:
 
         return Block("0", "0", dt.datetime.now(), 0, 0)
 
-    def make_nounce(self):
+    # def make_nounce(self):
         # """
         # A function that returns a nonce that is less than the target.
         # """
@@ -247,15 +188,15 @@ class Block:
         #     else:
         #         nonce += 1
         # return nonce
-        curr_nonce = random.randint(0, 2**64)
-        tries = 0
-        while True:
-            tries += 1
-            if int(hashlib.sha256((hex(curr_nonce) + self.hash_root.MerkleTree.getRootHash()).encode('utf-8')).hexdigest(), 16) < self.target:
-                self.nonce = curr_nonce
-                break
-            else:
-                curr_nonce = random.randint(0, 2**64)
+        # curr_nonce = random.randint(0, 2**64)
+        # tries = 0
+        # while True:
+        #     tries += 1
+        #     if int(hashlib.sha256((hex(curr_nonce) + self.hash_root.MerkleTree.getRootHash()).encode('utf-8')).hexdigest(), 16) < self.target:
+        #         self.nonce = curr_nonce
+        #         break
+        #     else:
+        #         curr_nonce = random.randint(0, 2**64)
 
 # def mine(self, block):
 #         for n in range(self.maxNonce):
@@ -271,22 +212,37 @@ class Block:
 
 
 class Blockchain:
+    max_nonce = 2**64
+    geneis_block = Block.create_genesis_block()
+    head = geneis_block
 
-    print("Begin Block\n")
-    print("Begin Header\n")
-    print("Genesis Block\n")
-    genesis_block = [Block.create_genesis_block()]
-    print("Hash: "+genesis_block[0].compute_hash() + "\n")
-    print("Hash of root: " + genesis_block[0].hash_prev + "\n")
-    print("Timestamp: " + str(genesis_block[0].timestamp) + "\n")
-    print("Target: " + str(genesis_block[0].target) + "\n")
-    print("Nonce: " + str(genesis_block[0].make_nounce()) + "\n")
-    print("End Header\n")
-    print("End Block\n")
-    print("------------------------\n")
+    def add_block(self, block):
+        # next should be the next input file
+        block.hash_prev = self.block.compute_hash()
+        self.block.next = block
+        self.block = self.block.next
 
-    # # nonce =
-    # # target =
+    def set_nonce(self, block):
+        for n in range(self.max_nonce):
+            if int(block.compute_hash(), 16) < self.target:
+                self.add_block(block)
+                break
+            else:
+                block.nonce += 1
+
+    def print_blockchain(self):
+        genesis_block = [Block.create_genesis_block()]
+        print("Begin Block\n")
+        print("Begin Header\n")
+        print("Genesis Block\n")
+        print("Hash: "+genesis_block[0].compute_hash() + "\n")
+        print("Hash of root: " + genesis_block[0].hash_prev + "\n")
+        print("Timestamp: " + str(genesis_block[0].timestamp) + "\n")
+        print("Target: " + str(genesis_block[0].target) + "\n")
+        print("Nonce: " + str(genesis_block[0].make_nounce()) + "\n")
+        print("End Header\n")
+        print("End Block\n")
+        print("------------------------\n")
 
     # # initialize the blockchain
     # def __init__(self):
@@ -313,13 +269,75 @@ class Blockchain:
 #         # # # adding the genesis block to the chain
 #         # # self.chain.append(genesis_block)
 
-#     def printBlock():
-#         print("BEGIN BLOCK")
-#         print("BEGIN HEADER")
-#         print("END HEADER")
-#         print("s")
+# read in each argument in argv adn append it to an array. then we have array of all filesname / paths to files.
 
 
-# output = Blockchain()
-# print(output)
-# print(genesis_block.compute_hash)
+# reads in fileInputs, which is the list of paths to the different input files
+def makeTree(fileInputs):
+    print("\nInputted files: ", fileInputs, "\n")
+    for files in fileInputs:
+        try:
+            # try - catch to try to open file
+            file = open(files, "r")
+            print('File opened')
+        except:
+            # if not a file, print this and exit program
+            print("\nError: Please enter a valid text file.\n")
+            sys.exit(0)    # exit program
+
+        array = []
+        for line in file:
+            array.append(line.strip())  # add each line to array
+
+        tree = MerkleTree(array)        # make tree from input array ()
+        print("Root Hash: " + tree.getRootHash() + "\n")
+        # tree.printTreeGraphically()
+
+
+# if "python3 allcode.py" only, then prompt for file inputs where user input starts
+size=0
+if len(sys.argv) == 1:
+    print("\nMake a selection \n1. Enter multiple file names \n2. Enter a folder (all files in folder will be entered) \n3. Exit\n")
+    selectionInput = input("Selection: ")
+    if (selectionInput == "1"):       # multiple file names
+        print("Enter a single file name one at a time (enter 0 to exit): ")
+        fileNames = []      # array to make tree
+        fileName = ""       # file location var
+        loop = True         # infinite loop unless 0 is entered
+        while (loop):
+            if (fileName == "0"):        # ends loop
+                loop = False
+            else:                       # enter values to be added to array. 0 will still be entered at the end, but is removed below
+                fileName = input("\nEnter the path to a file (0 to exit): ")
+                fileNames.append(fileName)
+        # remove last element from array (which would be 0 to exit loop but also got appended to array)
+        fileNames.pop()
+        makeTree(fileNames)
+    elif (selectionInput == "2"):     # folder
+        # prompt for folder name
+        print("Enter the name of the folder (must be in same directory): ")
+        folderName = input("Folder Name: ")    # get folder name
+        dirList = os.listdir(folderName)    # get list of files in folder
+        size=len(dirList)
+        # add folder name to each file name
+        dirList = ["{}/{}".format(folderName, i) for i in dirList]
+        makeTree(dirList)   # make tree from list of files
+    else:
+        print("Exiting...")    # exit program
+        sys.exit(0)    # exit program
+
+# Makes tree if more than 1 argv
+if len(sys.argv) > 1:
+    # make tree for every file by making an array of file inputs for the tree via argv (ignores argv[0] with is python3 and argv[1] which is program name, takes elements onwards)
+    size=len(sys.argv)-2
+    makeTree(sys.argv[1:])
+
+#def __init__(self, hash_prev, hash_root, timestamp, target, nonce):
+
+blockchain = Blockchain()
+for n in range(size):
+    blockchain.set_nonce(Block)
+
+while blockchain.head != None:
+    print(blockchain.head)
+    blockchain.head = blockchain.head.next
