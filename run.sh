eabc3556c2e27ef00b69ff14d9606bb4327a7aa9
Dan Unhuryan <62815005+danielu20101@users.noreply.github.com>
2022-09-29 00:00:16 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-# 10 test input files (im sorting them like this because this is how they get listed in the folder)
-file1='./input/1.txt'
-file2='./input/10.txt'
-file3='./input/2.txt'
-file4='./input/3.txt'
-file5='./input/4.txt'
-file6='./input/5.txt'
-file7='./input/6.txt'
-file8='./input/7.txt'
-file9='./input/8.txt'
-file10='./input/9.txt'
 # Run program
 cd ./Code
-python3 ./allcode.py $file1 $file2 $file3 $file4 $file5 $file6 $file7 $file8 $file9 $file10
+python3 ./allcode.py
