b56609869bced2e96c9e025388d79ac1ddbb70c6
Dan Unhuryan <unguryan77@gmail.com>
2022-09-29 00:47:24 -0400

    edited nonce to random and formatted nonce output

--- Code/allcode.py
+++ Code/allcode.py
@@ -154,7 +154,7 @@ class Block:
         self.hash_root = hash_root  # hash of root of Merkle tree
         self.timestamp = dt.datetime.now()  # timestamp
         self.target = 2 ** 255  # for the difficulty target
-        self.nonce = 0  # nonce
+        self.nonce = random.randint(0, 2**32)  # nonce
 
     def compute_hash(self):
         """
@@ -251,7 +251,6 @@ if len(sys.argv) == 1:
 # Makes tree if more than 1 argv
 if len(sys.argv) > 1:
     # make tree for every file by making an array of file inputs for the tree via argv (ignores argv[0] with is python3 and argv[1] which is program name, takes elements onwards)
-    # size=len(sys.argv)-2
     makeTree(sys.argv[1:])
 
 object2 = Blockchain()  # create blockchain object
@@ -272,6 +271,8 @@ for n in range(length):
         block = Block(blocksList[-1].compute_hash(), merkletree.getRootHash())
     blocksList.append(block)    # add block to list of blocks
 
+
+
 #print files in given format
 for i in range(len(blocksList)):
     print("Begin Block\n")          
@@ -280,9 +281,11 @@ for i in range(len(blocksList)):
     print("Hash of root: " + blocksList[i].hash_prev + "\n")
     print("Timestamp: " + str(blocksList[i].timestamp) + "\n")
     print("Target: " + str(blocksList[i].target) + "\n")
+    print("Nonce: " + str(blocksList[i].nonce) + "\n")
     print("END HEADER\n")
     print("END BLOCK\n")
     print("------------------------\n")
+
 
 os.mkdir("output")
 
@@ -297,6 +300,7 @@ for i in range(len(blocksList)):
     file.write("Hash of root: " + blocksList[i].hash_prev + "\n")
     file.write("Timestamp: " + str(blocksList[i].timestamp) + "\n")
     file.write("Target: " + str(blocksList[i].target) + "\n")
+    file.write("Nonce: " + str(blocksList[i].nonce) + "\n")
     file.write("END HEADER\n")
     file.write("END BLOCK\n")
     file.write("------------------------\n")
@@ -305,14 +309,3 @@ for i in range(len(blocksList)):
     for line in file_content:
         file.write(line)
     file.close()
-
-    #print out accounts and balances from txt file
-    # file = open(fileNames[i], 'r')
-    # for line in file:
-    #     print(line)
-    # file.close()
-
-
-
-    
-    # one sec lemme remember
