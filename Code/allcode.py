c866df3da56cdef7af7b157c97d7cc13442fc8a4
Steven Nazaroff <stnazaroff@yahoo.com>
2022-09-28 23:29:12 -0400

    changes

--- Code/allcode.py
+++ Code/allcode.py
@@ -28,7 +28,7 @@ class Leaf:
 class MerkleTree:
     def __init__(self, array):
         self.buildTree(array)
-
+    # we must be entering in a filename into MerkleTree() somewhere
     def buildTree(self, values):
         balance = []       # balance of the leafs
         account = []       # address of the leafs
@@ -48,26 +48,26 @@ class MerkleTree:
                 account.append(acct)    # add address to account array
                 balance.append(bal)     # add balance to balance array
             except:
-                print("Error while building tree. Values at error point printed.")
+                print("Error while building tree. Values at error point printed.") # no clue yet, its not an error with the inputs
+                print("Line: ", line)
                 print(line.split(" ", 1))
-                print(values)
                 sys.exit(-1)
 
         # create leafs from the values
         Leafs = [Leaf(None, None, account[i], balance[i], Leaf.hash(
             account[i] + balance[i])) for i in range(len(account))]     # create leafs
 
-        self.root = self._buildTree(Leafs)
+        self.root = self._buildTree(Leafs)  # build the tree
 
     def _buildTree(self, Leafs):
         half = len(Leafs) // 2    # get half of the length of the Leafs
         # create new Leafs from pairs of Leafs
         if len(Leafs) == 2:
             # return the new Leafs
-            return Leaf(Leafs[0], Leafs[1], None, None, Leaf.hash(Leafs[0].hashValue + Leafs[1].hashValue))
+            return Leaf(Leafs[0], Leafs[1], None, None, Leaf.hash(Leafs[0].hashValue + Leafs[1].hashValue)) # create new Leafs
         elif len(Leafs) == 1:
             # return the new Leafs
-            return Leaf(Leafs[0], None, (Leafs[0].address), (Leafs[0].balance), Leaf.hash(Leafs[0].hashValue))
+            return Leaf(Leafs[0], None, (Leafs[0].address), (Leafs[0].balance), Leaf.hash(Leafs[0].hashValue))  # create new Leafs
 
         # recursive call
         left = self._buildTree(Leafs[:half])    # left child
@@ -76,11 +76,11 @@ class MerkleTree:
         balance = None  # balance of the new leaf
         if right:
             # hash the new Leafs
-            hashValue = Leaf.hash(left.hashValue + right.hashValue)
+            hashValue = Leaf.hash(left.hashValue + right.hashValue) # hash the new Leafs
         else:
-            hashValue = Leaf.hash(left.hashValue)
+            hashValue = Leaf.hash(left.hashValue)   # hash the new Leafs
         # return the new Leafs
-        return Leaf(left, right, address, balance, hashValue)
+        return Leaf(left, right, address, balance, hashValue)   # create new Leafs
 
     def getRootHash(self):      # returns the root hash
         return self.root.hashValue
@@ -104,7 +104,7 @@ class MerkleTree:
 
     # prints tree in order
     def printTreeInOrder(self):
-        self._printTreeInOrder(self.root)
+        self._printTreeInOrder(self.root)   
 
     def _printTreeInOrder(self, Leaf):
         if Leaf != None:
@@ -134,12 +134,12 @@ class MerkleTree:
         # print the node with indentation
         print(' ' * 4 * level + '->', node, end=" ")
         if node.left is None and node.right is None:
-            print(f'\t[{node.address}, {node.balance}]')
+            print(f'\t[{node.address}, {node.balance}]')    # prints address and balance
         else:
             print()
-        self._printTreeGraphically(node.left, level + 1)
+        self._printTreeGraphically(node.left, level + 1)    # recursive call
 
-# --------------------------Begin HW 4-------------------------------------
+# ------------------------------Begin HW 4-------------------------------------
 
 # • hash of the header of the previous block (zero for the initial genesis block)
 # • hash of the root of the Merkle tree stored in the current block
@@ -147,15 +147,13 @@ class MerkleTree:
 # • difficulty target
 # • nonce
 
-
 class Block:
-    next = None
 
-    def __init__(self, hash_prev, hash_root, timestamp, target, nonce):
+    def __init__(self, hash_prev, hash_root):
         self.hash_prev = hash_prev  # hash of previous block
         self.hash_root = hash_root  # hash of root of Merkle tree
         self.timestamp = dt.datetime.now()  # timestamp
-        self.target = 2 ** (256-50)  # for the difficulty target
+        self.target = 2 ** 255  # for the difficulty target
         self.nonce = 0  # nonce
 
     def compute_hash(self):
@@ -163,118 +161,38 @@ class Block:
         A function that return the hash of the block contents.
         """
         hashes = hashlib.sha256(((str(self.hash_prev) + str(self.hash_root) + str(
-            self.timestamp) + str(self.target) + str(self.nonce)).encode('utf-8'))).hexdigest()
-        return hashes
+            self.timestamp) + str(self.target) + str(self.nonce)).encode('utf-8'))).hexdigest() # hash the block
+        return hashes   # return the hash
 
-    @staticmethod
-    def create_genesis_block():
-        """
-        A function to manually create a genesis block.
-        The previous hash is set to 0, the root of the hash is set to 0,
-        the timestamp is set to the current time, and the nonce and target
-        are set to 0.
-        """
-
-        return Block("0", "0", dt.datetime.now(), 0, 0)
-
-    # def make_nounce(self):
-        # """
-        # A function that returns a nonce that is less than the target.
-        # """
-        # nonce = 0
-        # while True:
-        #     if int(self.compute_hash(), 16) <= self.target:
-        #         break
-        #     else:
-        #         nonce += 1
-        # return nonce
-        # curr_nonce = random.randint(0, 2**64)
-        # tries = 0
-        # while True:
-        #     tries += 1
-        #     if int(hashlib.sha256((hex(curr_nonce) + self.hash_root.MerkleTree.getRootHash()).encode('utf-8')).hexdigest(), 16) < self.target:
-        #         self.nonce = curr_nonce
-        #         break
-        #     else:
-        #         curr_nonce = random.randint(0, 2**64)
-
-# def mine(self, block):
-#         for n in range(self.maxNonce):
-#             if int(block.hash(), 16) <= self.target:
-#                 self.add(block)
-#                 print(block)
-#                 break
-#             else:
-#                 block.nonce += 1
-    # You will need to find a nonce such that the nonce concatenated
-    # with the root hash of the Merkle tree is hashed by SHA-256 to a value less than or equal to the specified
-    # target. Please set your target such that the probability of success is 50%
-
-
-class Blockchain:
-    max_nonce = 2**64
-    geneis_block = Block.create_genesis_block()
-    head = geneis_block
-
-    def add_block(self, block):
-        # next should be the next input file
-        block.hash_prev = self.block.compute_hash()
-        self.block.next = block
-        self.block = self.block.next
+class Blockchain ():
+    def __init__(self):
+        self.max_nonce = 2 ** 32    # max nonce
+        self.target = 2 ** 255  # target
 
     def set_nonce(self, block):
+        object1 = Block(block.hash_prev, block.hash_root)   # create a new block
+        target = random.randint(0, 2**32)   # random target
+
         for n in range(self.max_nonce):
-            if int(block.compute_hash(), 16) < self.target:
-                self.add_block(block)
-                break
+            if int(object1.compute_hash(), 16) < self.target:
+                self.max_nonce = target   # set max nonce
+                break   # break
             else:
-                block.nonce += 1
+                object1.nonce = random.randint(0, 2**32)    # set nonce
 
-    def print_blockchain(self):
-        genesis_block = [Block.create_genesis_block()]
-        print("Begin Block\n")
-        print("Begin Header\n")
-        print("Genesis Block\n")
-        print("Hash: "+genesis_block[0].compute_hash() + "\n")
-        print("Hash of root: " + genesis_block[0].hash_prev + "\n")
-        print("Timestamp: " + str(genesis_block[0].timestamp) + "\n")
-        print("Target: " + str(genesis_block[0].target) + "\n")
-        print("Nonce: " + str(genesis_block[0].make_nounce()) + "\n")
-        print("End Header\n")
-        print("End Block\n")
-        print("------------------------\n")
+    def create_genesis_block(self):
+        return Block("0", "0")  # create genesis block
 
-    # # initialize the blockchain
-    # def __init__(self):
-    #     self.chain = list()
-    #     genesis_block = self.createblocks("0", "0", dt.datetime.now(), 0, 0)
-    #     self.chain.append(genesis_block)
+# read in each rgument in argv adn append it to an array. then we have array of all filesname / paths to files.
 
-    # def createblocks(self, hash_prev, hash_root, timestamp, target, nonce):
-    #     self.hash_prev = hash_prev
-    #     self.hash_roor = hash_root
-    #     self.timestamp = dt.dataTime.now()
-    #     self.target = target
-    #     self.nonce = nonce
-
-
-#         # genesis_block = Block("Genesis")
-#         # timestamp = datetime.datetime.now()
-#         # hash_prev = 0
-#         # return (Block(genesis_block, hash_prev, 0, timestamp, 0, 0))
-
-#         # genesis_block = Block("Genesis")
-#         # # genesis_block = Block(0, 0, [], 0, "0")  # create the genesis block
-#         # # genesis_block.hash = genesis_block.compute_hash()  # hashing for the genesis block
-#         # # # adding the genesis block to the chain
-#         # # self.chain.append(genesis_block)
-
-# read in each argument in argv adn append it to an array. then we have array of all filesname / paths to files.
-
-
+firstFileName = "test"
 # reads in fileInputs, which is the list of paths to the different input files
 def makeTree(fileInputs):
-    print("\nInputted files: ", fileInputs, "\n")
+    print("\nInputted files: ", fileInputs, "\n")   # print inputted files
+    firstFileName = fileInputs[0]
+    print(":: ", fileInputs[0])
+    print(firstFileName)
+    array = []  # array of all the transactions
     for files in fileInputs:
         try:
             # try - catch to try to open file
@@ -285,23 +203,25 @@ def makeTree(fileInputs):
             print("\nError: Please enter a valid text file.\n")
             sys.exit(0)    # exit program
 
-        array = []
+        #array = []
         for line in file:
             array.append(line.strip())  # add each line to array
 
         tree = MerkleTree(array)        # make tree from input array ()
         print("Root Hash: " + tree.getRootHash() + "\n")
         # tree.printTreeGraphically()
+    return array
 
 
 # if "python3 allcode.py" only, then prompt for file inputs where user input starts
-size=0
+size = 0
+fileNames = []
 if len(sys.argv) == 1:
     print("\nMake a selection \n1. Enter multiple file names \n2. Enter a folder (all files in folder will be entered) \n3. Exit\n")
     selectionInput = input("Selection: ")
     if (selectionInput == "1"):       # multiple file names
         print("Enter a single file name one at a time (enter 0 to exit): ")
-        fileNames = []      # array to make tree
+        #fileNames = []      # array to make tree
         fileName = ""       # file location var
         loop = True         # infinite loop unless 0 is entered
         while (loop):
@@ -312,15 +232,20 @@ if len(sys.argv) == 1:
                 fileNames.append(fileName)
         # remove last element from array (which would be 0 to exit loop but also got appended to array)
         fileNames.pop()
+        # remove empty strings from array
+        fileNames = list(filter(None, fileNames))
+        length = len(fileNames)
         makeTree(fileNames)
     elif (selectionInput == "2"):     # folder
         # prompt for folder name
         print("Enter the name of the folder (must be in same directory): ")
         folderName = input("Folder Name: ")    # get folder name
         dirList = os.listdir(folderName)    # get list of files in folder
-        size=len(dirList)
+        #size = len(dirList)
         # add folder name to each file name
         dirList = ["{}/{}".format(folderName, i) for i in dirList]
+        length = len(dirList)
+        fileNames = dirList
         makeTree(dirList)   # make tree from list of files
     else:
         print("Exiting...")    # exit program
@@ -329,15 +254,59 @@ if len(sys.argv) == 1:
 # Makes tree if more than 1 argv
 if len(sys.argv) > 1:
     # make tree for every file by making an array of file inputs for the tree via argv (ignores argv[0] with is python3 and argv[1] which is program name, takes elements onwards)
-    size=len(sys.argv)-2
+    # size=len(sys.argv)-2
     makeTree(sys.argv[1:])
 
-#def __init__(self, hash_prev, hash_root, timestamp, target, nonce):
+object2 = Blockchain()  # create blockchain object
+block = None    # block object
+blocksList = []    # list of blocks
 
-blockchain = Blockchain()
-for n in range(size):
-    blockchain.set_nonce(Block)
+for n in range(length):
+    if n == 0:
+        block = object2.create_genesis_block()  # create genesis block
+        object2.set_nonce(block)    # set nonce
+    elif(selectionInput == "2"): 
+        tree = makeTree(dirList)    # make tree
+        merkletree = MerkleTree(tree)    # this should be array of contents of the file, not file names    
+        block = Block(blocksList[-1].compute_hash(), merkletree.getRootHash())  # create block
+    else:
+        tree = makeTree(fileNames)  # make tree
+        merkletree = MerkleTree(tree)    # this should be array of contents of the file, not file names    
+        block = Block(blocksList[-1].compute_hash(), merkletree.getRootHash())
+    blocksList.append(block)    # add block to list of blocks
 
-while blockchain.head != None:
-    print(blockchain.head)
-    blockchain.head = blockchain.head.next
+#print files in given format
+for i in range(len(blocksList)):
+    print("Begin Block\n")          
+    print("Begin Header\n") 
+    print("Hash: "+blocksList[i].compute_hash() + "\n")
+    print("Hash of root: " + blocksList[i].hash_prev + "\n")
+    print("Timestamp: " + str(blocksList[i].timestamp) + "\n")
+    print("Target: " + str(blocksList[i].target) + "\n")
+    print("END HEADER\n")
+    print("END BLOCK\n")
+    print("------------------------\n")
+
+os.mkdir("output")
+
+
+for i in range(len(blocksList)):
+    tempFileName = os.path.basename(dirList[i])
+    tempFileName = tempFileName[:-4] 
+    file = open('output/' + tempFileName + '.block.out', 'w')
+    file.write("Begin Block\n")
+    file.write("Begin Header\n")
+    file.write("Hash: "+blocksList[i].compute_hash() + "\n")
+    file.write("Hash of root: " + blocksList[i].hash_prev + "\n")
+    file.write("Timestamp: " + str(blocksList[i].timestamp) + "\n")
+    file.write("Target: " + str(blocksList[i].target) + "\n")
+    file.write("END HEADER\n")
+    file.write("END BLOCK\n")
+    file.write("------------------------\n") 
+    ## write accouts and balances
+
+    
+
+
+
+# Ok lets print accounts & balances then idk how to do that
